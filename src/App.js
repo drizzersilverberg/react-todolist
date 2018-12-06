f883bdd5a984e918b3c2e38d51b4511b9edfc7a1
Dendi Handian <dhn@voxteneo.asia>
2018-12-06 14:31:10 +0700

    Todo Component placeholder

--- src/App.js
+++ src/App.js
@@ -1,12 +1,65 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+
+// components
+import Todo from './components/Todo'
+
+// material-ui components
+import { withStyles } from '@material-ui/core/styles';
+import Grid from '@material-ui/core/Grid';
+import Paper from '@material-ui/core/Paper';
+
+const styles = theme => ({
+  root: {
+    flexGrow: 1,
+  },
+  demo: {
+    height: 240,
+  },
+  paper: {
+    padding: theme.spacing.unit * 2,
+    height: '100%',
+    color: theme.palette.text.secondary,
+  },
+  control: {
+    padding: theme.spacing.unit * 2,
+  },
+});
 
 class App extends Component {
   render() {
+    const {classes} = this.props;
     return (
       <div>
+        <Grid container className={classes.root}>
+          <Grid item xs={12}>
+            <Grid
+              container
+              justify="center"
+              alignItems="center"
+              direction="row"
+              spacing={16}
+              className={classes.demo}
+            >
+              <Grid
+                item
+                xs={10}
+                sm={8}
+                md={8}
+                lg={6}
+              >
+                <Todo/>
+              </Grid>
+            </Grid>
+          </Grid>
+        </Grid>
       </div>
     );
   }
 }
 
-export default App;
+App.PropTypes = {
+  classes: PropTypes.object.isRequired,
+};
+
+export default withStyles(styles)(App);
---
+++ src/components/Todo.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+
+// material-ui components
+import { withStyles } from '@material-ui/core/styles';
+import Paper from '@material-ui/core/Paper';
+
+class Todo extends React.Component {
+  render () {
+    return (
+      <div className="">
+        <Paper>
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ut quod numquam, quaerat facilis veniam beatae harum repellat perferendis sit, dolorem, quas. Exercitationem a quidem, repudiandae totam praesentium, eius inventore ipsum!
+        </Paper>
+      </div>
+    )
+  }
+}
+
+export default Todo;
